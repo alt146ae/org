4f8ffb926e55c85dac7e2df9fd405cdfa2b8f0b0
Emanuel <emanuel.cortezm@gmail.com~>
2024-06-10 12:52:58 -0600

    inicio

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/Header';
+console.log(Header)
 
 function App() {
+  let suma = 3+8
   return (
     <div className="App">
+        <Header />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hola mundo con React
         </p>
         <a
           className="App-link"
@@ -15,7 +19,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Aprende React
         </a>
       </header>
     </div>
